f944f6c9c6bd35a2274cec7cadccf256d9581a33
toxemik <toxemik@gmail.com>
2021-10-12 21:35:23 +0200

    add ui controls: previous, next, page number, menu toggler

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const uiControlsObject = document.querySelector('.uiControls');
+const uiElements = document.querySelectorAll('.ui');
 // the stop variable is useful to get a single onmousemove event
 // at a time
 let stop = false;
@@ -6,9 +6,11 @@ let stop = false;
 document.querySelector('body').onmousemove = () => {
     if (!stop) {
         stop = true;
-        uiControlsObject.style.visibility = 'visible';
+        for(const element of uiElements)
+            element.style.visibility = 'visible';
         setTimeout(() => {
-            uiControlsObject.style.visibility = 'hidden';
+            for(const element of uiElements)
+                element.style.visibility = 'hidden';
             stop = false;
         }, 1500);
     }
