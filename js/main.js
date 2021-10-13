a3ddff377131859245adc3d7033ef6ed0162da50
toxemik <toxemik@gmail.com>
2021-10-13 21:29:27 +0200

    change the content when a menu item is clicked

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+const data = {
+    tatooine: {
+        img: "images/f27903e009_50179578_tatooine-lucasfilm.jpeg",
+        title: "La planète-désert Tatooine",
+        description: "Tatooine est une planète-désert de l’univers de fiction Star Wars. Située dans la Bordure extérieure, cette planète orbite autour des étoiles binaires Tatoo I et II. Il s'agit du monde d'origine de la famille Skywalker."
+    },
+    mosEspa: {
+        img: "images/1600px-Star_Wars_Episode_One_Village_-_05.jpeg",
+        title: "Mos Espa",
+        description: "Avant que l'avènement de l'Empire ne contraigne le seigneur du crime Jabba le Hutt à transférer ses activités, Mos Espa est la plus grande ville de Tatooine, et d'une certaine façon, sa capitale."
+    },
+    tatooineTraders: {
+        img: "images/Star_Tours_-_The_Adventure_Continues_(29380304448).jpeg",
+        title: "Tatooine Traders",
+        description: "La boutique Tatooine Traders à la sortie de <i>Star Tours: The Adventures Continue</i> à Walt Disney World Resort."
+    },
+    systemesBinaires: {
+        img: "images/1600px-Kepler-16.jpeg",
+        title: "Systèmes binaires",
+        description: "Tatooine a marqué la culture par l'iconique scène du coucher de soleil binaire dans <i>Un nouvel espoir</i> sorti en 1977. En effet, le film y présente le concept d'objet circumbinaire, dont le premier a été découvert en 1993. Depuis, les exoplanètes découvertes dans des systèmes binaires sont parfois surnommées « Tatooine »."
+    },
+    quartierEsclaves: {
+        img: "images/Ksar_Ouled_Soltane_01.jpeg",
+        title: "Quartier des Esclaves",
+        description: "Lieu de tournage du quartier des esclaves de Mos Espa où résident Shmi et Anakin Skywalker dans la prélogie."
+    }
+}
+
 // get the .ui elements
 const uiElements = document.querySelectorAll('.ui:not(.menuPanel, .menuToggler)');
 // get the .menuPanel element
@@ -32,5 +60,25 @@ document.querySelector('.menuToggler').onclick = () => {
     } else {
         menuTogglerElement.style.visibility = 'hidden';
         menuPanelVisible = false;
+    }
+}
+
+// get the current page name
+let currentPage = 'tatooine';
+
+// load the content related to the page arg (menu item)
+function loadContent(page) {
+    for(const content in data) {
+        if(content == page) {
+            // change the current page name
+            currentPage = page
+            // load the content
+            document.querySelector('img').setAttribute('src', data[page].img);
+            document.querySelector('.title').innerHTML = data[page].title;
+            document.querySelector('.description').innerHTML = data[page].description;
+            // hide the menu
+            menuTogglerElement.style.visibility = 'hidden';
+            menuPanelVisible = false;
+        }
     }
 }
