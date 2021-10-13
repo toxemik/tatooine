6f4198a146257e6e6adfdfc19c867d751956cad5
toxemik <toxemik@gmail.com>
2021-10-13 19:02:58 +0200

    add menu toggler event to show the menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
-const uiElements = document.querySelectorAll('.ui');
+// get the .ui elements
+const uiElements = document.querySelectorAll('.ui:not(.menuPanel, .menuToggler)');
+// get the .menuPanel element
+const menuTogglerElement = document.querySelector('.menuPanel');
+
 // the stop variable is useful to get a single onmousemove event
 // at a time
 let stop = false;
 
+// on the mouse move event, show ui commands
 document.querySelector('body').onmousemove = () => {
     if (!stop) {
         stop = true;
@@ -13,5 +18,19 @@ document.querySelector('body').onmousemove = () => {
                 element.style.visibility = 'hidden';
             stop = false;
         }, 1500);
+    }
+}
+
+// the menu panel state (visible | hidden)
+let menuPanelVisible = false;
+
+// on the mouse click event on the menu toggler, show the menu panel
+document.querySelector('.menuToggler').onclick = () => {
+    if(!menuPanelVisible) {
+        menuTogglerElement.style.visibility = 'visible';
+        menuPanelVisible = true;
+    } else {
+        menuTogglerElement.style.visibility = 'hidden';
+        menuPanelVisible = false;
     }
 }
